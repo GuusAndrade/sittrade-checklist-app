dd9f663499db695c2c42f57bfe84e738abc6b64d
GuusAndrade <guustavo.andrade04@gmail.com>
2025-12-18 21:59:48 -0300

    feat(checklist): implement checklist event classes for state management

---
+++ lib/src/modules/checklist/presentation/bloc/checklist_event.dart
@@ -0,0 +1,34 @@
+import 'package:equatable/equatable.dart';
+
+abstract class ChecklistEvent extends Equatable {
+  const ChecklistEvent();
+
+  @override
+  List<Object?> get props => [];
+}
+
+class ToggleChecklistItem extends ChecklistEvent {
+  final String itemId;
+
+  const ToggleChecklistItem(this.itemId);
+
+  @override
+  List<Object?> get props => [itemId]; 
+}
+
+class AddPhotoToItem extends ChecklistEvent {
+  final String itemId;
+  final String photoPath;
+
+  const AddPhotoToItem({
+    required this.itemId,
+    required this.photoPath,
+  });
+
+  @override
+  List<Object?> get props => [itemId, photoPath];
+}
+
+class SubmitChecklist extends ChecklistEvent {
+  const SubmitChecklist();
+}
